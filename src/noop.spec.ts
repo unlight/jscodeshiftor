101f08977b9836073d5fdf37112c153f76e65e06
Roman <s91630277@gmail.com>
2024-11-16 22:59:49 +0400

    chore: Nothing special

--- src/noop.spec.ts
+++ src/noop.spec.ts
@@ -2,12 +2,12 @@ import expect from 'expect';
 import { it, describe } from 'mocha';
 
 import plugin from './noop';
-import { runPlugin } from './testing';
+import { runTransform } from './testing';
 
 describe('noop', () => {
   it('noop', () => {
-    const result = runPlugin(plugin, `const foo = 1`);
+    const result = runTransform(plugin, `const foo = 1`);
 
-    expect(result).toEqual('const foo = 1');
+    expect(result.lines).toEqual(['const foo = 1']);
   });
 });
